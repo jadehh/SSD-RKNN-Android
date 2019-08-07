60482c5590bfc86f40fecae8626d4f3fb8f3edee
jade <jadehh@live.com>
2019-08-07 10:59:52 +0800

    环境搭建

--- app/src/main/java/com/example/ssd_rknn_android/MainActivity.java
+++ app/src/main/java/com/example/ssd_rknn_android/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.libssddetect.JavaBridgeSSD;
+
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.IOException;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        JavaBridgeSSD javaBridgeSSD = new JavaBridgeSSD(this,R.raw.ssd_mobilenet_v1_hand_tensorflow);
+        javaBridgeSSD.loadOpencv();
+        try{
+            javaBridgeSSD.jTools.copyAssertFile(this,"videos","video_2019-07-05_15-09-37.avi");
+        }catch (IOException e){
+
+        }
+
     }
 }
--- libssddetect/src/main/java/com/example/libssddetect/JavaBridgeSSD.java
+++ libssddetect/src/main/java/com/example/libssddetect/JavaBridgeSSD.java
@@ -1,21 +1,69 @@
 package com.example.libssddetect;
+import android.app.Activity;
+import android.util.Log;
+
+import com.example.jade.JadeLog;
+import com.example.jade.jade_tools;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
 import static com.example.libssddetect.PostProcess.*;
 public class JavaBridgeSSD {
     public static final String TAG = "ssd";
     private String mModelName = "ssd.rknn";
-
+    public jade_tools jTools;
 
     private String fileDirPath;     // file dir to store model cache
     private InferenceWrapper mInferenceWrapper;
+    private Activity mActivity;
 
+    public JavaBridgeSSD(Activity activity,int id){
+        fileDirPath = activity.getCacheDir().getAbsolutePath();
+        jTools = new jade_tools();
+        jTools.copyRawFile(activity,fileDirPath,mModelName,id);
+        mActivity = activity;
 
-    public void Inference(){
+    }
+
+    public void Inference(byte[] data){
         int count = 0;
         long oldTime = System.currentTimeMillis();
         long currentTime;
         String paramPath = fileDirPath + "/" + mModelName;
         mInferenceWrapper = new InferenceWrapper(INPUT_SIZE,INPUT_CHANNEL,NUM_RESULTS,NUM_CLASSES,paramPath);
-        mInferenceWrapper.run()
+        mInferenceWrapper.run(data);
+    }
 
+
+    //Load opencv的回调函数
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(mActivity) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i("OpenCV", "OpenCV loaded successfully");
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+    /**
+     * 加载openc库
+     */
+    public void loadOpencv() {
+        if (!OpenCVLoader.initDebug()) {
+            JadeLog.e("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, mActivity, mLoaderCallback);
+        } else {
+            JadeLog.e("OpenCV", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
     }
 }
