f2cb1fc135326060414a0c006d83f0c022fe9754
jadehh <jadehh@live.com>
2019-08-22 11:21:22 +0800

    新增手势人脸检测模型

--- opencv41Libs/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
+++ opencv41Libs/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\jadeh\\Destop\\SSD-RKNN-Android\\opencv41Libs\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Administrator\\AndroidStudioProjects\\Github\\SSD-RKNN-Android\\opencv41Libs\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
